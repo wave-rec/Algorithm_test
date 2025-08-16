631bbff08d3f1541b1afca3035f3df0075f9686a
SY_AHN <werther0704@gmail.com>
2025-08-16 15:13:17 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,16 +1,12 @@
 N = int(input())
 M = input()
-M_list = []
-for i in M:
-    M_list.append(i)
 
-
-first = N*int(M_list[2])
-second = N*int(M_list[1])
-third = N*int(M_list[0])
+first = N*int(M[2])
+second = N*int(M[1])
+third = N*int(M[0])
 
 
 print(first)
 print(second)
 print(third)
-print(first + second*10 + third*100)
+print(N*int(M))
