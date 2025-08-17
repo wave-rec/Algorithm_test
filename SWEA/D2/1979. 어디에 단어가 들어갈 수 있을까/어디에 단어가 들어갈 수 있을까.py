177d6845b8d635253cee38d96d1a832abbc7de5a
SY_AHN <werther0704@gmail.com>
2025-08-17 22:17:05 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 68 ms, Memory: 53,376 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,37 +1,30 @@
 T = int(input())
 
-for tc in range(1, 1+T):
+for tc in range(1, T+1):
     N, K = map(int, input().split())
-    quiz_map = [list(map(int, input().split())) for _ in range(N)]
-    length_of_quiz = 0
+    puzzle = [list(map(int, input().split())) for _ in range(N)]
+    ans = 0
 
-    # 가로
-    for row in range(N):
-        count = 0
-        for col in range(N):
-            if quiz_map[row][col] == 1:
-                count += 1
-            else:
-                if count == K:
-                    length_of_quiz += 1
-                count = 0
-        # 행 끝에서 한 번 더
-        if count == K:
-            length_of_quiz += 1
 
-    # 세로
-    for col in range(N):
-        count = 0
-        for row in range(N):
-            if quiz_map[row][col] == 1:
-                count += 1
-            else:
-                if count == K:
-                    length_of_quiz += 1
-                count = 0
-        # 열 끝에서 한 번 더
-        if count == K:
-            length_of_quiz += 1
+    for i in puzzle:
+        row_str = ''.join(map(str, i))
+        gaps = row_str.split('0')
 
-    # 출력
-    print(f'#{tc} {length_of_quiz}')
+        for gap in gaps:
+            if len(gap) == K:
+                ans += 1
+
+
+    for i in range(N):
+        col = []
+        for j in range(N):
+            col.append(puzzle[j][i])
+
+        col_str = ''.join(map(str, col))
+        gaps = col_str.split('0')
+
+        for gap in gaps:
+            if len(gap) == K:
+                ans += 1
+
+    print(f'#{tc} {ans}')
