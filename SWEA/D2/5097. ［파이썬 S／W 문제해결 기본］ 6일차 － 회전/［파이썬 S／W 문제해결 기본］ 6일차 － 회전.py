f653978836c6460bd07fe84419350169e3c3726c
SY_AHN <werther0704@gmail.com>
2025-08-20 14:51:43 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 6일차 - 회전, Time: 68 ms, Memory: 53,888 KB -BaekjoonHub

--- SWEA/D2/5097. ［파이썬 S／W 문제해결 기본］ 6일차 － 회전/［파이썬 S／W 문제해결 기본］ 6일차 － 회전.py
+++ SWEA/D2/5097. ［파이썬 S／W 문제해결 기본］ 6일차 － 회전/［파이썬 S／W 문제해결 기본］ 6일차 － 회전.py
@@ -1,12 +1,12 @@
+from collections import deque
 T = int(input())
 
 for tc in range(1, T+1):
     N, M = map(int, input().split())
     numbers = list(map(int, input().split()))
+    q = deque(numbers)
 
     for _ in range(M):
-        first_num = numbers[0]
-        numbers.pop(0)
-        numbers.append(first_num)
+        q.append(q.popleft())
 
-    print(f'#{tc} {numbers[0]}')
+    print(f'#{tc} {q[0]}')
