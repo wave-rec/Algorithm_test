bbcfec8500a2271f85446fac67e6588aed81b4d0
SY_AHN <werther0704@gmail.com>
2025-09-01 01:09:00 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 5일차 - 미로, Time: 80 ms, Memory: 60,160 KB -BaekjoonHub

--- SWEA/D2/4875. ［파이썬 S／W 문제해결 기본］ 5일차 － 미로/［파이썬 S／W 문제해결 기본］ 5일차 － 미로.py
+++ SWEA/D2/4875. ［파이썬 S／W 문제해결 기본］ 5일차 － 미로/［파이썬 S／W 문제해결 기본］ 5일차 － 미로.py
@@ -2,33 +2,34 @@ def escape():
     while s:
         a, b = s.pop()
         maze[a][b] = 1
+        for w in range(4):
+            c = a + dx[w]
+            d = b + dy[w]
 
-        for i in range(4):
-            o = a + dx[i]
-            p = b + dy[i]
-
-            if 0 <= o < N and 0 <= p < N:
-                if maze[o][p] == 0:
-                    maze[o][p] = 1
-                    s.append([o, p])
-                elif maze[o][p] == 3:
+            if 0 <= c < N and 0 <= d < N:
+                if maze[c][d] == 0:
+                    s.append([c,d])
+                    maze[c][d] = 1
+                elif maze[c][d] == 3:
                     return 1
     return 0
 
 
 T = int(input())
+
 for tc in range(1, T+1):
     N = int(input())
     maze = [list(map(int, input().strip())) for _ in range(N)]
 
-    dx = [-1, 0, 1, 0]
-    dy = [0, -1, 0, 1]
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, -1, 1]
     s = []
 
     for i in range(N):
         for j in range(N):
             if maze[i][j] == 2:
-                s.append([i, j])
+                s.append([i,j])
                 break
 
     print(f'#{tc} {escape()}')
+
