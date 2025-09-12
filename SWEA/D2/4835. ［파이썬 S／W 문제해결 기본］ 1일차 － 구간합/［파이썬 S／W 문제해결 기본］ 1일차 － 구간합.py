a99e8d9762ff94c57209a86a8f8956d1073fdc87
SY_AHN <werther0704@gmail.com>
2025-09-12 11:49:57 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 1일차 - 구간합, Time: 62 ms, Memory: 58,240 KB -BaekjoonHub

--- SWEA/D2/4835. ［파이썬 S／W 문제해결 기본］ 1일차 － 구간합/［파이썬 S／W 문제해결 기본］ 1일차 － 구간합.py
+++ SWEA/D2/4835. ［파이썬 S／W 문제해결 기본］ 1일차 － 구간합/［파이썬 S／W 문제해결 기본］ 1일차 － 구간합.py
@@ -1,20 +1,19 @@
-T = int(input()) #테스트 케이스 개수
-for tc in range(1, T+1): # 케이스 별로 돌면서
-    N, M = map(int, input().split()) # 배열 길이, 연속으로 더할 개수 저장
-    arr = list(map(int, input().split())) # 주어진 숫자 배열 리스트로 저장
+T = int(input())
 
-    cur = sum(arr[0:M]) #첫 구간의 합을 구해서 cur에 저장
-    max_cur = cur #여태 나온 값 중 최댓값
-    min_cur = cur #여태 나온 값 중 최솟값
+for tc in range(1, T+1):
+    n, m = map(int, input().split())
+    numbers = list(map(int, input().split()))
 
-    for i in range(0, N - M + 1): #시작 인덱스를 처음부터 N-M까지 돌면서
-        start = arr[i : i+M] #i번째 구간은 arr[i] 부터 arr[i+M]까지
-        sum_cur = sum(start) #해당 구간의 합
-
-        if sum_cur > max_cur: #해당하는 구간의 합이 여태 최댓값보다 크면
-            max_cur = sum_cur #갱신
-
-        if sum_cur < min_cur: #해당하는 구간의 합이 여태 최솟값보다 작으면
-            min_cur = sum_cur #갱신
-
-    print(f'#{tc} {max_cur - min_cur}') #최댓값 - 최솟값 출력
+    max_sum = 0
+    min_sum = 28e8
+    
+    for i in range(n-m+1):
+        sum_cur = sum(numbers[i:i+m])
+        
+        if sum_cur > max_sum:
+            max_sum = sum_cur
+            
+        if sum_cur < min_sum:
+            min_sum = sum_cur
+            
+    print(f'#{tc} {max_sum-min_sum}')
