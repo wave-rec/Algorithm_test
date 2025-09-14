45b0fbba2b5260e174043f5a28e9596221486c83
SY_AHN <werther0704@gmail.com>
2025-09-14 23:40:32 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 3일차 - 화물 도크, Time: 63 ms, Memory: 52,864 KB -BaekjoonHub

--- SWEA/D3/5202. ［파이썬 S／W 문제해결 구현］ 3일차 － 화물 도크/［파이썬 S／W 문제해결 구현］ 3일차 － 화물 도크.py
+++ SWEA/D3/5202. ［파이썬 S／W 문제해결 구현］ 3일차 － 화물 도크/［파이썬 S／W 문제해결 구현］ 3일차 － 화물 도크.py
@@ -2,18 +2,15 @@ T = int(input())
 
 for tc in range(1, T+1):
     N = int(input())
-    info = [list(map(int, input().split()))for _ in range(N)]
+    times = [list(map(int, input().split())) for _ in range(N)]
 
-    sorted_info = sorted(info, key=lambda x: x[1])
+    times.sort(key=lambda x: x[1])
 
-    cnt = 0
-    now = 0
+    last = 0
+    ans = 0
+    for s, e in times:
+        if last <= s:
+            ans += 1
+            last = e
 
-    for i in range(N):
-        s = sorted_info[i][0]
-        e = sorted_info[i][1]
-        if now <= s:
-            cnt += 1
-            now = e
-
-    print("#{} {}".format(tc, cnt ))
+    print(f'#{tc} {ans}')
