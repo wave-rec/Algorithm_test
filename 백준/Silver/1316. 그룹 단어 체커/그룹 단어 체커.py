2e56d57032ee94f4aceba1b0bb40d782710eeaff
안수연 (Suyeon Ahn) <werther0704@gmail.com>
2026-02-25 23:38:54 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+cnt = n
+
+for i in range(n):
+    word = input()
+    for j in range(0, len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+
+print(cnt)
