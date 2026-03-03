55ff991793811ca379356defb4ef13595c49c00a
안수연 (Suyeon Ahn) <werther0704@gmail.com>
2026-03-03 17:38:36 +0900

    [Silver V] Title: 영화감독 숌, Time: 792 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+
+N = int(input().strip())
+
+cnt = 0
+num = 666
+while True:
+    str_num = str(num)
+    if "666" in str_num:
+        cnt += 1
+
+    if N == cnt:
+        break
+    num += 1
+
+print(num)
